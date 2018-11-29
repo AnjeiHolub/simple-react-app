ef42d4fd5d07efad24092ce75eb6250551ec8f51
Andrzej Holub <aholubprof@gmail.com>
2018-11-29 22:58:07 +0300

    Second commit

---
+++ build/precache-manifest.5109c679ce95cf8eb55d78910d9490a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d785244241c780a5435",
+    "url": "/static/js/main.3d785244.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "3d785244241c780a5435",
+    "url": "/static/css/main.300852c8.chunk.css"
+  },
+  {
+    "revision": "1be44c47c17bb6b4149c8fcbe7204a0a",
+    "url": "/index.html"
+  }
+];
